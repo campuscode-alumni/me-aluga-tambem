01c03ba43a042db690a8680f3d8590df87602d5c
Carla Crude e Lucas Sapienza <carlacrude@gmail.com>
2018-09-26 22:09:07 -0300

    Usuario se registra com sucesso
    Co-authored-by: Carla Crude <carlacrude@gmail.com>
    Co-authored-by: Lucas Sapienza <sapienzalucas@gmail.com>

--- spec/features/user/user_register_spec.rb
+++ spec/features/user/user_register_spec.rb
@@ -6,16 +6,20 @@ require 'rails_helper'
       visit root_path
       click_on 'Cadastrar usuario'
       fill_in 'Nome', with: 'José'
-      fill_in 'Email', with: 'Jose@email.com'
+      fill_in 'Email', with: 'jose@email.com'
       fill_in 'Senha', with: '123456'
       fill_in 'Confirmação de senha', with: '123456'
       fill_in 'Documento', with: '123'
       fill_in 'Telefone', with: '123'
       click_on 'Cadastrar'
 
-      # expect(page).to have_css('div.alert.alert-success', text: 'Seu cadastro foi efetuado com sucesso')
-      expect(page).to have_content('oskopaske')
+      user = User.last
+      expect(user.name).to eq('José')
+      expect(user.email).to eq('jose@email.com')
+      expect(user.document).to eq('123')
+      expect(user.phone).to eq(123)
 
+      expect(page).to have_content('You have signed up successfully')
     end
   
   end
